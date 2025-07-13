449b774aeb2c709e93f48939528a29bf030dc7d1
KSam001 <samcapital.inc@gmail.com>
2025-07-13 11:54:36 -0700

    Fix 7-multi_c.js to handle negative numbers and update README.md

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,8 @@
 const count = Number.parseInt(process.argv[2]);
 
-if (Number.isNaN(count) || count < 0) {
+if (Number.isNaN(count)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (count > 0) {
   for (let i = 0; i < count; i++) {
     console.log("C is fun");
   }
